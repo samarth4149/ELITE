f6a4e63de45eebd90118b1df732d318e4c04dc46
samarth <samarth4149@gmail.com>
2023-11-03 15:38:44 -0400

    added more jobs to the running jobs list

--- submitit_gen_domainnet.py
+++ submitit_gen_domainnet.py
@@ -37,6 +37,11 @@ if __name__ == '__main__':
         'elite_global_s2p_job_9',
         'elite_global_c2p_job_0',
         'elite_global_c2p_job_3',
+        'elite_global_s2c_job_8',
+        'elite_global_s2c_job_9',
+        'elite_global_s2p_job_3',
+        'elite_global_s2p_job_6',
+        'elite_global_s2c_job_1',
     ]
 
     for src, tgt in itertools.permutations(domains, 2):
