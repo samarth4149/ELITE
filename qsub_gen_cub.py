00bea590e61409703f0c80bc067b2d492594479c
samarth <samarth4149@gmail.com>
2023-11-04 10:53:35 -0400

    ready to generate some img2img data for CUB

--- qsub_gen_cub.py
+++ qsub_gen_cub.py
@@ -14,33 +14,33 @@ dataset = 'cub'
 domains = ['Real', 'Painting']
 
 for src, tgt in itertools.permutations(domains, 2):
-    for job_idx in range(NUM_JOBS):
-        strength = 0.5
-        root_dir = '/projectnb/ivc-ml/samarth/projects/synthetic/data/synthetic-cdm/synthetic_data/elite_global_img2img/strength_{strength}'
-        scenario = f'{src[0]}2{tgt[0]}'
-        expt_name = f'elite_global_img2img_strength_{strength}_{dataset}_{scenario}_job_{job_idx}'
-        proc_arr = ['qsub']
-        proc_arr.extend(get_qsub_options(
-            qsub_name=expt_name,
-            outfile=Path(root_dir) / dataset / scenario / f'qsub_log_{job_idx}.txt',
-            project=PROJECT,
-            duration='11:50:00',
-            # gpu_c='4.5',
-            gpu_memory='20G',
-        ))
+    for strength in [0.3, 0.7]:
+        for job_idx in range(NUM_JOBS):
+            root_dir = '/projectnb/ivc-ml/samarth/projects/synthetic/data/synthetic-cdm/synthetic_data/elite_global_img2img/strength_{strength}'
+            scenario = f'{src[0]}2{tgt[0]}'
+            expt_name = f'elite_global_img2img_strength_{strength}_{dataset}_{scenario}_job_{job_idx}'
+            proc_arr = ['qsub']
+            proc_arr.extend(get_qsub_options(
+                qsub_name=expt_name,
+                outfile=Path(root_dir) / dataset / scenario / f'qsub_log_{job_idx}.txt',
+                project=PROJECT,
+                duration='11:50:00',
+                # gpu_c='4.5',
+                gpu_memory='20G',
+            ))
 
-        proc_arr += ['python', 'gen_data.py']
-        proc_arr += ['--dataset', dataset]
-        proc_arr += ['--source', src]
-        proc_arr += ['--target', tgt]
-        proc_arr += ['--root_dir', root_dir]
-        proc_arr += ['--batch_size', '12']
-        proc_arr += ['--num_jobs', str(NUM_JOBS)]
-        proc_arr += ['--job_idx', str(job_idx)]
-        
-        proc_arr += ['--strength', str(strength)]
-        
-        
-        print('Job name : ', expt_name)
-        print('Command :', ' '.join(proc_arr))
-        os.system(' '.join(proc_arr))
+            proc_arr += ['python', 'gen_data.py']
+            proc_arr += ['--dataset', dataset]
+            proc_arr += ['--source', src]
+            proc_arr += ['--target', tgt]
+            proc_arr += ['--root_dir', root_dir]
+            proc_arr += ['--batch_size', '12']
+            proc_arr += ['--num_jobs', str(NUM_JOBS)]
+            proc_arr += ['--job_idx', str(job_idx)]
+            
+            proc_arr += ['--strength', str(strength)]
+            
+            
+            print('Job name : ', expt_name)
+            print('Command :', ' '.join(proc_arr))
+            os.system(' '.join(proc_arr))
